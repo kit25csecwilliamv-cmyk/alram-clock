c788578048d456ce51ad5b96e0c3e9c4e19f8a7f
kit25csecwilliamv-cmyk <kit25csecwilliam.v@gmail.com>
2026-09-19 19:06:50 +0530

    Add alarm sound playback functionality

--- sound_manager.py
+++ sound_manager.py
@@ -1,1 +1,18 @@
+import pygame
+import os
 
+pygame.mixer.init()
+
+
+def play_alarm():
+    sound_file = "sounds/alarm1.mp3"
+
+    if os.path.exists(sound_file):
+        pygame.mixer.music.load(sound_file)
+        pygame.mixer.music.play(-1)
+    else:
+        print("Alarm sound not found!")
+
+
+def stop_alarm():
+    pygame.mixer.music.stop()
